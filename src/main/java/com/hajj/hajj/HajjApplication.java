9965325921b1822116aea39eb86a03f97c36815e
Fedila Minewer <fedila.m@zamzambank.com>
2024-01-12 10:34:28 +0300

    get hujjaj detail is worked

--- src/main/java/com/hajj/hajj/HajjApplication.java
+++ src/main/java/com/hajj/hajj/HajjApplication.java
@@ -1,7 +1,14 @@
 package com.hajj.hajj;
 
+import com.hajj.hajj.model.HUjjaj;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class HajjApplication {
