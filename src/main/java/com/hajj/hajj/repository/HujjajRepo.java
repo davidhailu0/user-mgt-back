b1b93fa0ae506f095082520917b410574352b15c
Dawit Birhane <dawit.b@zamzambank.com>
2024-01-16 15:59:29 +0300

    added additional end points

--- src/main/java/com/hajj/hajj/repository/HujjajRepo.java
+++ src/main/java/com/hajj/hajj/repository/HujjajRepo.java
@@ -5,11 +5,15 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface HujjajRepo extends JpaRepository<HUjjaj, Long> {
 
     @Query(value = "SELECT h from HUjjaj h where h.payment_code = :payment_code")
     Optional<HUjjaj> findHUjjajByPaymentCode(@Param("payment_code") String payment_code);
+
+    @Query(value = "SELECT h from HUjjaj h where h.paid = :status")
+    List<HUjjaj> findHUjjajByPaidStatus(@Param("status") boolean status);
 
 }
