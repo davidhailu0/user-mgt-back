4487dc6c813dce17f98041d0ba9d2df0b2c2cb46
Fedila Minewer <fedila.m@zamzambank.com>
2024-01-16 09:47:35 +0300

    this name query api done

--- src/main/java/com/hajj/hajj/HajjApplication.java
+++ src/main/java/com/hajj/hajj/HajjApplication.java
@@ -14,55 +14,6 @@ import java.util.Map;
 public class HajjApplication {
 
 	public static void main(String[] args) {
-
-		String amount="200";
-		String  draccount="0010934520101";
-		String  naration= "transa";
-		String paymentcode="ZQYAYW";
-
-		String apiUrl="http://10.10.11.20/HajjFundTransfer/1.0.0/";
-		String token="eyJ4NXQiOiJNV0l5TkRJNVlqRTJaV1kxT0RNd01XSTNOR1ptTVRZeU5UTTJOVFZoWlRnMU5UTTNaVE5oTldKbVpERTFPVEE0TldFMVlUaGxNak5sTldFellqSXlZUSIsImtpZCI6Ik1XSXlOREk1WWpFMlpXWTFPRE13TVdJM05HWm1NVFl5TlRNMk5UVmhaVGcxTlRNM1pUTmhOV0ptWkRFMU9UQTROV0UxWVRobE1qTmxOV0V6WWpJeVlRX1JTMjU2IiwidHlwIjoiYXQrand0IiwiYWxnIjoiUlMyNTYifQ.eyJzdWIiOiI0NThkN2I0My0xOTU0LTRlNjQtYjI0NS1iNDg4YjAyYWNmNmMiLCJhdXQiOiJBUFBMSUNBVElPTiIsImF1ZCI6IlRJNHJlUXlheERiYUxhQVVfVWViMDk4d1ZJa2EiLCJuYmYiOjE3MDUwNjMzMzUsImF6cCI6IlRJNHJlUXlheERiYUxhQVVfVWViMDk4d1ZJa2EiLCJzY29wZSI6ImRlZmF1bHQiLCJpc3MiOiJodHRwczpcL1wvMTkyLjE2OC4xLjI5Ojk0NDhcL29hdXRoMlwvdG9rZW4iLCJleHAiOjEwMzQ1MDYzMzM1LCJpYXQiOjE3MDUwNjMzMzUsImp0aSI6IjdmZTFmZTYwLTllNGItNDMyNy04MmM3LTVhOTU1Mjg4YjBjMyIsImNsaWVudF9pZCI6IlRJNHJlUXlheERiYUxhQVVfVWViMDk4d1ZJa2EifQ.eQncaeeMZy_lCNan_dmWCm9ib_C9xFNAk0tYUfQQEhDO9J0tLvCDCQD-LhsPQWoLNNq6oCciQjC1vZnqTU2Tim0ws5zlEVIA_dUxNVv0-kzDShwQKbseasuVk2Czdsom7ucz9VJbtUf23P2pBbZ-mMlW4PSofBpn2mab6z1wl48Q3-0AdknC5wC8YjpHO69Ptk_MkKINecz4LIbQ_zNRCcLMs6sFX0tBmoiBrSV6HhyGlJ1Lu2bCejoRhtmmV-DWoDpH4FpViMerLWy1lV0qccHlLh1lwdS-3IsNdt0PJmL4lrJYefQ8fxBBDI04HSA0PwBAN8dm3hWim3hgqYizXg";
-		RestTemplate restTemplate=new RestTemplate();
-		HttpHeaders headers=new HttpHeaders();
-		headers.set("Authorization", "Bearer " + token);
-		headers.setContentType(MediaType.APPLICATION_JSON);
-		String jsonBody = "{ " +
-				"\"Amount\":" +"\""+ amount + "\"" + ","+
-		        "\"Draccount\":" +"\""+ draccount + "\"" + ","+
-				"\"Narrative\": " +"\""+ naration + "\"" + ","+
-				"\"PaymentCode\": " +"\""+ paymentcode + "\"" +
-
-				"}";
-		HttpEntity<String> requestEntity = new HttpEntity<>(jsonBody,headers);
-		try {
-			ResponseEntity<Map<String, Object>> responseEntity = restTemplate.exchange(apiUrl, HttpMethod.POST, requestEntity, new ParameterizedTypeReference<Map<String, Object>>() {});
-
-			Map<String, Object> responseBody = responseEntity.getBody();
-
-			if (responseBody != null) {
-				Object MSGSTAT = responseBody.get("MSGSTAT");
-				if ("FAILURE".equals(MSGSTAT)) {
-					Object EDESC = responseBody.get("EDESC");
-					String name = EDESC.toString();
-					System.out.println("EDESC: " + name);
-				}
-				else
-				{
-
-			}
-			}
-			else {
-				System.out.println("Response body is empty or null");
-			}
-		}
-		catch (HttpClientErrorException ex) {
-			// Handle unauthorized error
-		  ex.getMessage();
-		}
-
-
-
-
 		SpringApplication.run(HajjApplication.class, args);
 	}
 
--- src/main/java/com/hajj/hajj/controller/HajjController.java
+++ src/main/java/com/hajj/hajj/controller/HajjController.java
@@ -35,6 +35,47 @@ public class HajjController {
 
     private final ObjectMapper objectMapper = new ObjectMapper();
 
+    @GetMapping("/get_nameQuery/{account_number}")
+    public  Object get_nameQuery(@PathVariable String account_number) {
+
+        final String name_Query_api = env.getProperty("name_Query")+ account_number;
+		String token=env.getProperty("wso2Token");
+        RestTemplate restTemplate=new RestTemplate();
+        HttpHeaders headers =new HttpHeaders();
+        headers.set("Authorization", "Bearer " + token);
+
+        HttpEntity<String> requestEntity = new HttpEntity<>(headers);
+        try {
+            ResponseEntity<Map<String, Object>> responseEntity = restTemplate.exchange(name_Query_api, HttpMethod.GET, requestEntity, new ParameterizedTypeReference<Map<String, Object>>() {});
+
+            Map<String, Object> responseBody = responseEntity.getBody();
+
+            if (responseBody != null) {
+                Object status = responseBody.get("status");
+                if ("FAILURE".equals(status)) {
+                    Map<String, Object> error = new HashMap<>();
+                    error.put("message", responseBody.get("error_description"));
+                    error.put("status", "Error");
+                    return error;
+
+
+                }
+                else
+                {
+                    return responseBody;
+                }
+            }
+            else {
+                return  "Response body is empty or null";
+            }
+        }
+        catch (HttpClientErrorException ex) {
+            // Handle unauthorized error
+           return ex.getMessage();
+        }
+
+    }
+
     @GetMapping("/get_hujaj/{payment_code}")
     public  Object get_hujaj(@PathVariable String payment_code)
     {
